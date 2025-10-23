11a5e9ae0ddedbb6316dbb77e999e8a687c22335
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-10-23 08:28:18 -0600

    feat(api): add User model routes and enable database sync
    - Added GET / route to list all users
    - Added GET /create-user route to insert a sample user
    - Added sequelize.sync({ force: true }) to recreate tables
    - Temporarily disabled birds route

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require("express")
 const birdsRoute = require("./routes/birds.route.js")
 const sequelize = require("./database/sequelize-connect.js")
-
+const User = require("./models/user.model.js")
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
+app.get('/', async (req, res) => {
+    const users = await User.findAll();
+    res.send(users)
+})
+
+app.get('/create-user', async (req, res) => {
+    const user = await User.create({ firstName: "amir", lastName: "jalali", password:"1234" , email: "amir@g.com"})
+    res.send(user)
 })
 
 // Mount birds routes under /birds endpoint
-app.use('/birds', birdsRoute)
+// app.use('/birds', birdsRoute)
 
 app.listen(port, async() => {
     try {
         // Test database connection before starting the server
         await sequelize.authenticate()
+        await sequelize.sync({force:true})
         console.log("Connected To Database Successfully!")
-
         console.log(`Web Application Listening On Port ${port}`)
     } catch (error) {
         // If database connection fails, show an error message
