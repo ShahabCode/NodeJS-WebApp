85d16bfea5e4b4d088e2725192fac302f0e3307b
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-10-23 08:02:56 -0600

    Edit comments

--- routes/birds.route.js
+++ routes/birds.route.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
+// Define a simple route for the home page
 router.get('/', (req, res) => {
     res.send('Home Page')
 })
 
-// Export the User model for use in other files
+// Export the router so it can be used in app.js
 module.exports = router
