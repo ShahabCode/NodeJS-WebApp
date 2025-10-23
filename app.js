e0ca4ac4b15a82908c53807851e3253f664633ee
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-10-23 08:51:53 -0600

    feat(auth): add login endpoint and JSON parsing middleware
    - Added express.json() middleware to parse incoming JSON requests
    - Implemented POST /login endpoint for user authentication
    - Cleaned up route formatting for better readability

--- app.js
+++ app.js
@@ -4,19 +4,43 @@ const sequelize = require("./database/sequelize-connect.js")
 const User = require("./models/user.model.js")
 const app = express()
 const port = 3000
+app.use(express.json())
+
 
 app.get('/', async (req, res) => {
     const users = await User.findAll();
     res.send(users)
 })
 
+
 app.get('/create-user', async (req, res) => {
     const user = await User.create({ firstName: "amir", lastName: "jalali", password:"1234" , email: "amir@g.com"})
     res.send(user)
 })
 
+
+app.post('/login', async (req, res) => {
+    const {email, password} = req.body
+    const user = await User.findOne({
+        where: {
+            email
+        }
+    })
+    if (user) {
+        if (user.password == password){
+            res.send("Hi To Panel!")
+        } else {
+            res.send("The Password is Incorrent")
+        }
+    } else {
+        res.send("User Not Found!")
+    }
+})
+
+
 // Mount birds routes under /birds endpoint
 // app.use('/birds', birdsRoute)
+
 
 app.listen(port, async() => {
     try {
