6a1c97273b31245904ad886d371a10884ca760aa
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-10-26 02:58:54 -0600

    feat: improve login error handling and add user registration route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const User = require("../models/user.model.js")
 
 router.get("/login", (req, res) => {
       res.render("login") // ./viws/login.ejs
@@ -7,28 +8,45 @@ router.get("/login", (req, res) => {
  
 
 router.post("/login", async (req, res) => {
-    const {email, password} = req.body
-    const user = await User.findOne({
-        where: {
-            email
-        }
-    })
-    if (user) {
-        if (user.password == password){
-            res.send("Hi To Panel!")
-        } else {
-            res.send("The Password is Incorrent")
-        }
-    } else {
-        res.send("User Not Found!")
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+        return res.render("login", { error: "User not found!" });
+    }
+
+    if (user.password !== password) {
+        return res.render("login", { error: "Password is incorrect!" });
     }
 })
 
 
 router.get('/register', async (req, res) => {
-    const user = await User.create({ firstName: "amir", lastName: "jalali", password:"1234" , email: "amir@g.com"})
-    res.send(user)
+    res.render("register")
 })
+
+
+router.post('/register', async (req, res) => {
+    const {firstName, lastName , email, password} = req.body
+    const user = await User.findOne({
+        where: {
+            email
+        }
+    })
+    if (user){
+        res.render("register", {"error": "User Already Exists!"})
+    } else {
+        const user = await User.create({
+            firstName,
+            lastName,
+            password,
+            email
+        })
+
+        res.send("User Registred!")
+    }
+})
+
 
 // Export the router so it can be used in app.js
 module.exports = router
