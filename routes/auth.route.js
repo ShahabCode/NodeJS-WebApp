50f95946bd50675280dd30ba407e6b0a9372df38
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-10-29 12:45:44 -0600

    feat: implement JWT-based login authentication and set token in cookies

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const User = require("../models/user.model.js")
+const jwt = require("jsonwebtoken")
 
 router.get("/login", (req, res) => {
       res.render("login") // ./viws/login.ejs
@@ -10,6 +11,7 @@ router.get("/login", (req, res) => {
 router.post("/login", async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
+    const secretKey = "secret"
 
     if (!user) {
         return res.render("login", { error: "User not found!" });
@@ -17,6 +19,20 @@ router.post("/login", async (req, res) => {
 
     if (user.password !== password) {
         return res.render("login", { error: "Password is incorrect!" });
+    } else {
+        const payload = {
+            "id": user._id,
+            "email": user.email
+        }
+        const token = jwt.sign(payload, secretKey, {
+            expiresIn: "1h"
+        })
+        res.cookie("token", token, {
+            maxAge: 3 * 60 * 60 * 1 * 1000,
+            path: '/'
+        })
+
+        res.redirect("/user/profile")
     }
 })
 
