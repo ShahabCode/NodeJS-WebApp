e8bef03f46361a8f6d1f82c6897ac29d8a055f49
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-11-07 04:57:28 -0700

    feat: add auth middleware for JWT token verification

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken")
+
+const requireAuth = (req, res, next) => {
+    const {token} = req.cookies
+    const secretKey = "secret"
+
+    if (token) {
+        try{
+            const decoded = jwt.verify(token, secretKey)
+            if (decoded) {
+                res.locals.decoded = decoded
+                next()
+            } else {
+                res.redirect("/auth/login")
+            }
+        
+        } catch (error) {
+        res.redirect("/auth/login")
+        } 
+    } else {
+        res.redirect("/auth/login")
+    }
+}
+
+module.exports = {
+    requireAuth
+}
