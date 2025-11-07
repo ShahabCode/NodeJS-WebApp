32a5967093d091420ed0752b4b7c61a140140401
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-11-07 04:56:38 -0700

    refactor: move JWT verification to auth middleware for profile route

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,32 +2,21 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/user.model.js")
 const jwt = require("jsonwebtoken")
+const { requireAuth } = require("../middlewares/auth.middleware.js")
 
-router.get("/profile", async (req, res) => {
-    const {token} = req.cookies
-    const secretKey = "secret"
-
-    if (token) {
-        try{
-            const decoded = jwt.verify(token, secretKey)
-            if (decoded) {
-                const user = await User.findOne({
-                    where: {
-                        email: decoded.email
-                    }
-                })
-                if (user) {
-                    res.render("profile", {user}) // ./viws/profile.ejs
-                } else {
-                    res.redirect("/auth/login")
-                }
-            }
-        } catch (error) {
-            console.log(error)
-            res.redirect("/auth/login")
+router.get('/profile', requireAuth, async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            email: res.locals.decoded.email
         }
-             
+    })
+
+    console.log(user)
+
+    if (user) {
+        res.render('profile', { user })
     } else {
+        res.redirect("/auth/login")
     }
 })
 
