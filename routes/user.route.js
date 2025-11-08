625da39277404779e5cdfa9991f3808f699dea4f
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-11-08 06:22:08 -0700

    feat: add password update functionality to user profile route

--- routes/user.route.js
+++ routes/user.route.js
@@ -36,17 +36,7 @@ router.get("/profile", requireAuth, async (req, res) => {
 router.post("/profile", requireAuth, upload.single("profile_image"), async (req, res) => {
     const profile_image = req.file?.originalname
     const { firstName, lastName, bio } = req.body
-    
-    await User.update(
-        req.body,
-        {
-             where: {
-                email: res.locals.decoded.email
-             }
-            }
-        )
-    res.redirect("/user/profile")
-
+    const { current_password, new_password, confirm_new_password } = req.body
 
     if (profile_image) {
         const static_path = `/user-files/${profile_image}`
@@ -61,6 +51,44 @@ router.post("/profile", requireAuth, upload.single("profile_image"), async (req,
                  }
             }
         )
+    } else if (current_password && new_password && confirm_new_password){
+        const user = await User.findOne({
+            where: {
+                email: res.locals.decoded.email
+            }
+        })
+
+        if (current_password == user.password){
+            if (new_password == confirm_new_password){
+                await User.update(
+                {
+                    password: new_password
+                },
+                {
+                    where: {
+                        email: res.locals.decoded.email
+                    }
+                }
+            )
+            res.redirect("/auth/login")
+
+            } else {
+                res.redirect("/user/profile")
+            }
+        } else {
+            res.redirect("/user/profile")
+        }
+
+    } else {
+        await User.update(
+            req.body,
+            {
+                 where: {
+                    email: res.locals.decoded.email
+                 }
+            }
+            )
+        res.redirect("/user/profile")
     }
 })
     
