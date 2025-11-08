2d405554a2dc59d194d8a582a3d96e7b14c5dd20
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-11-08 05:55:30 -0700

    feat: add profile update and image upload functionality to user routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,15 +3,27 @@ const router = express.Router()
 const User = require("../models/user.model.js")
 const jwt = require("jsonwebtoken")
 const { requireAuth } = require("../middlewares/auth.middleware.js")
+const multer = require("multer")
+const { where } = require("sequelize")
 
-router.get('/profile', requireAuth, async (req, res) => {
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "static/user-files/") // Set the directory for storing uploaded files
+    }, 
+    filename: function (req, file, cb) {
+        cb(null, file.originalname) // Keep the original filename
+    }
+})
+const upload = multer({storage:storage})
+
+
+router.get("/profile", requireAuth, async (req, res) => {
     const user = await User.findOne({
         where: {
             email: res.locals.decoded.email
         }
     })
-
-    console.log(user)
 
     if (user) {
         res.render('profile', { user })
@@ -20,4 +32,36 @@ router.get('/profile', requireAuth, async (req, res) => {
     }
 })
 
+
+router.post("/profile", requireAuth, upload.single("profile_image"), async (req, res) => {
+    const profile_image = req.file?.originalname
+    const { firstName, lastName, bio } = req.body
+    
+    await User.update(
+        req.body,
+        {
+             where: {
+                email: res.locals.decoded.email
+             }
+            }
+        )
+    res.redirect("/user/profile")
+
+
+    if (profile_image) {
+        const static_path = `/user-files/${profile_image}`
+
+        await User.update(
+            {
+               profile_image: static_path
+            }, 
+            {
+                 where: {
+                    email: res.locals.decoded.email
+                 }
+            }
+        )
+    }
+})
+    
 module.exports = router
