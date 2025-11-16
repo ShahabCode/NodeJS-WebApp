b7925db5fa5c1ca4f3219bb473ac0660127c356a
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-11-16 01:54:48 -0700

    feat(home): show latest posts on homepage and add search param `q`

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,17 +1,35 @@
 const express = require("express")
 const router = express.Router()
 const User = require("../models/user.model.js")
+const Post = require("../models/post.model.js")
 
 router.get("/", async (req, res) => {
     var users = await User.findAll({})
-    if (req.query.search){
+    const posts = await Post.findAll({
+        limit:10, order: [["createdAt", "DESC"]] 
+    })
+
+    if (req.query.q){
         var users = await User.findAll({
             where: {
-                firstName: req.query.search
+                firstName: req.query.q
             }
         })
     } 
-    res.render("home", {users})
+    res.render("home", {
+        users, posts: posts.map((item, index) => {
+            return {
+                id: item.id,
+                title: item.title,
+                cover: item.cover,
+                content: item.content.substring(0, 50)
+            }
+        })
+    })
 }) 
+ 
+// router.get("home/posts", async (req, res) => {
+   
+// })
 
 module.exports = router
