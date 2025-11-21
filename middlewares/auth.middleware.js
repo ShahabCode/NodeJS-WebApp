72a46b69e5c06f821fb5dcc0df934cc9a1f6eb21
SHAHAB(Epic Resurge) <shahabepic@gmail.com>
2025-11-21 10:12:55 -0700

    feat: add isAdmin middleware to verify admin role

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,4 +1,35 @@
 const jwt = require("jsonwebtoken")
+const User = require('../models/user.model');
+
+const isAdmin = async (req, res, next) => {
+    const secretKey = 'secret';
+    const { token } = req.cookies
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, secretKey);
+
+            if (decoded) {
+                const user = await User.findOne({where: {email: decoded.email}})
+
+                if (user.role == "admin") {
+                    next();
+                } else {
+                    res.redirect("/")
+                }
+                
+            } else {
+                res.redirect("/auth/login")
+            }
+        } catch (error) {
+            console.log(error)
+            res.redirect("/auth/login")
+        }
+    } else {
+        res.redirect("/auth/login")
+    }
+}
+
 
 const requireAuth = (req, res, next) => {
     const {token} = req.cookies
@@ -23,5 +54,6 @@ const requireAuth = (req, res, next) => {
 }
 
 module.exports = {
-    requireAuth
+    requireAuth,
+    isAdmin
 }
